49c8668750b53b4e4ee0b5cc348c26738634476e
Worshox <worshox.28@gmail.com>
2023-02-01 19:26:20 +0100

    Some renaming

--- commands/pokaz-pingi.js
+++ commands/pokaz-pingi.js
@@ -9,18 +9,20 @@ module.exports = {
         .setDescription('Wyświetla listę aktualnie pingowanych ról przy odcinkach')
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
     async execute(interaction) {
+        const client = interaction.client;
+
         let rolesContent = '';
         for (const singlePing in videoPings) {
-            rolesContent = rolesContent.concat(`Rola: ${roleMention(videoPings[singlePing][1])}; slug: ${videoPings[singlePing][0]}\n`);
+            rolesContent += `Rola: ${roleMention(videoPings[singlePing][1])}; slug: ${videoPings[singlePing][0]}\n`;
         }
 
         const videoPingsEmbed = new EmbedBuilder()
             .setColor(0x950A0A)
             .setTitle('Pingowane role odcinków:')
-            .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL()})
+            .setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
             .setDescription(rolesContent || 'Brak pingowanych ról')
             .setTimestamp()
-            .setFooter({ text: 'AnimeNi', iconURL: 'https://animeni.pl/wp-content/uploads/2022/12/logo-v2.png' });
+            .setFooter({ text: 'AnimeNi', iconURL: client.user.displayAvatarURL() });
 
         await interaction.reply({ embeds: [videoPingsEmbed] });
     },
