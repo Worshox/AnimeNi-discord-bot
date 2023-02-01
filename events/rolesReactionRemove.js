e078bbca81d0371b99f4becc92d959de111cdd34
Worshox <worshox.28@gmail.com>
2023-02-02 00:23:12 +0100

    Add roles on reaction

---
+++ events/rolesReactionRemove.js
@@ -0,0 +1,30 @@
+const { Events } = require('discord.js');
+const reactionRoles = require('../config/reaction-roles.json');
+
+module.exports = {
+    name: Events.MessageReactionRemove,
+    async execute(reaction, user){
+        if (reaction.message.id !== reactionRoles.messageID) return; 
+        
+        if (reaction.partial) {
+            try {
+                await reaction.fetch();
+            } catch (error){
+                console.log('Nastąpił problem przy fetchowaniu wiadomości: ', error);
+                return;
+            }
+        }
+
+        const guild = reaction.message.guild;
+        const member = guild.members.cache.get(user.id);
+
+        let role;
+        for (const singleRole in reactionRoles)
+            if (reactionRoles[singleRole][0].includes(reaction.emoji.name))
+                role = reaction.message.guild.roles.cache.find(role => role.id === singleRole);
+
+        if (!role) return;
+        
+        member.roles.remove(role);
+    },
+};
