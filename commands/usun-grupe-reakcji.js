6b7ca0da4e0ae5189605ea7c738ef99246b3e04d
Worshox <worshox.28@gmail.com>
2023-02-06 13:12:35 +0100

    Add last reaction roles function

---
+++ commands/usun-grupe-reakcji.js
@@ -0,0 +1,42 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { PermissionFlagsBits } = require('discord.js');
+const { inlineCode } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const reactionRoles = require('../config/reaction-roles.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('usun-grupe-reakcji')
+        .setDescription('Usuwa podaną grupę reakcji razem z jej rolami reakcji, oraz jej wiadomość')
+        .addStringOption(option => option
+            .setName('grupa')
+            .setDescription('Grupa reakcji, którą chcesz usunąć')
+            .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+    async execute(interaction) {
+        const client = interaction.client;
+        const groupName = interaction.options.getString('grupa');
+
+        if (groupName in reactionRoles === false) {
+            await interaction.reply('Podana grupa nie istnieje w grupach roli reakcji.');
+            return;
+        }
+
+        const channel = client.channels.cache.get(reactionRoles[groupName].channelID);
+        channel.messages.fetch(reactionRoles[groupName].messageID)
+        .then(message => {
+            message.delete();
+
+            delete reactionRoles[groupName];
+
+            const reactionRolesFile = path.resolve(__dirname, '../config/reaction-roles.json');
+            fs.writeFile(reactionRolesFile, JSON.stringify(reactionRoles), (error) => {
+                if (error) console.log(error);
+            });
+        });
+
+
+        await interaction.reply(`Grupę ${inlineCode(groupName)} pomyślnie usunięto!`);
+    },
+};
