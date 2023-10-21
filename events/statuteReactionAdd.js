b1301c9ec6820cf18cf57beade83e997c8964999
Worshox <worshox.28@gmail.com>
2023-10-21 14:11:53 +0200

    delete statute reaction handling

--- events/statuteReactionAdd.js
+++ events/statuteReactionAdd.js
@@ -1,28 +1,28 @@
-const { Events } = require('discord.js');
-const { userRoleID, messageID } = require('../config/ruleset.json');
+// const { Events } = require('discord.js');
+// const { userRoleID, messageID } = require('../config/ruleset.json');
 
-module.exports = {
-    name: Events.MessageReactionAdd,
-    async execute(reaction, user){
-        if (reaction.message.id === messageID && reaction.emoji.name !== '✅') {
-            reaction.remove();
-            return;
-        }
-        if (reaction.message.id !== messageID || reaction.emoji.name !== '✅') return;
-        
-        if (reaction.partial) {
-            try {
-                await reaction.fetch();
-            } catch (error){
-                console.log('Nastąpił problem przy fetchowaniu wiadomości: ', error);
-                return;
-            }
-        }
+// module.exports = {
+//     name: Events.MessageReactionAdd,
+//     async execute(reaction, user){
+//         if (reaction.message.id === messageID && reaction.emoji.name !== '✅') {
+//             reaction.remove();
+//             return;
+//         }
+//         if (reaction.message.id !== messageID || reaction.emoji.name !== '✅') return;
 
-        const guild = reaction.message.guild;
-        const member = guild.members.cache.get(user.id);
-        const role = reaction.message.guild.roles.cache.get(userRoleID);
+//         if (reaction.partial) {
+//             try {
+//                 await reaction.fetch();
+//             } catch (error){
+//                 console.log('Nastąpił problem przy fetchowaniu wiadomości: ', error);
+//                 return;
+//             }
+//         }
 
-        member.roles.add(role);
-    },
-};
+//         const guild = reaction.message.guild;
+//         const member = guild.members.cache.get(user.id);
+//         const role = reaction.message.guild.roles.cache.get(userRoleID);
+
+//         member.roles.add(role);
+//     },
+// };
